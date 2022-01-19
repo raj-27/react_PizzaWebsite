fe41ddbd9b44487a7585abf50f77c8f400189e43
Raj vinod Yadav <raj819314@gmai.com>
2022-01-19 10:59:02 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDom from "react-dom";
 import'./index.css'
 // import App2 from './App2';
 // import About from './Pages2/About';
-// import App3 from "./App3";
-import App4 from "./App4";
+import App3 from "./App3";
+// import App4 from "./App4";
 
-ReactDom.render(<App4/>, document.getElementById("root"));
+ReactDom.render(<App3/>, document.getElementById("root"));
